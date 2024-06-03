daf5ab97caab0d230c3e8024211002164e5d912f
Manoj S <134809278+manojscoder@users.noreply.github.com>
2024-06-03 05:35:48 +0530

    Create Day_3.java

---
+++ Day_3.java
@@ -0,0 +1,16 @@
+// Problem link: 
+// Time: O(n)
+// Space: O(1)
+class Solution {
+    public int appendCharacters(String s, String t) {
+        int index = 0, t_length = t.length();
+
+        for(char c : s.toCharArray()) {
+            if(index < t_length && t.charAt(index) == c) {
+                index++;
+            }
+        }
+
+        return t_length - index;
+    }
+}
