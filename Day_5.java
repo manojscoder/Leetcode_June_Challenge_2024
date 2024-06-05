9fcee2765fb9c1e09cd103df039a4d294ee031fb
Manoj S <134809278+manojscoder@users.noreply.github.com>
2024-06-05 20:11:49 +0530

    Update Day_5.java

--- Day_5.java
+++ Day_5.java
@@ -1,33 +1,33 @@
 // Problem link: https://leetcode.com/problems/find-common-characters/description
-// Time: O(n)
-// Space: O(n)
+// Time: O(n * m), where n is the length of the words array, and m is average length of all words in words array
+// Space: O(1)
 class Solution {
     public List<String> commonChars(String[] words) {
-        char[][] store = new char[words.length][26];
         List<String> result = new ArrayList<>();
-        char chr;
-        int val;
+        int[] temp, store = new int[26];
 
-        for(int i = 0; i < words.length; i++) {
-            for(char c: words[i].toCharArray()) {
-                store[i][c - 'a']++;
+        for(char c : words[0].toCharArray()) {
+            store[c - 'a']++;
+        }
+
+        for(int i = 1; i < words.length; i++) {
+            temp = new int[26];
+
+            for(char c : words[i].toCharArray()) {
+                temp[c - 'a']++;
+            }
+
+            for(int j = 0; j < 26; j++) {
+                store[j] = Math.min(store[j], temp[j]);
             }
         }
 
         for(int i = 0; i < 26; i++) {
-            chr = (char)((int)'a' + i);
-            val = Integer.MAX_VALUE;
-
-            for(int j = 0; j < words.length; j++) {
-                val = Math.min(val, store[j][chr - 'a']);
-            }
-
-            for(int j = 0; j < val; j++) {
-                result.add(chr + "");
+            for(int j = 0; j < store[i]; j++) {
+                result.add((char)((int)'a' + i) + "");
             }
         }
 
         return result;
-
     }
 }
