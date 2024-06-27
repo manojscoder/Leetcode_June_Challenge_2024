00c16e7b07881801d255f640a466a28526b95360
Manoj S <134809278+manojscoder@users.noreply.github.com>
2024-06-27 11:30:50 +0530

    Create Day_27.java

---
+++ Day_27.java
@@ -0,0 +1,10 @@
+// Problem link: https://leetcode.com/problems/find-center-of-star-graph/description/
+// Time: O(1)
+// Space: O(1)
+class Solution {
+    public int findCenter(int[][] edges) {
+        if(edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1])
+            return edges[0][0];
+        return edges[0][1];
+    }
+}
